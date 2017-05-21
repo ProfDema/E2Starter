5c89140ed32755f37fec343d2c8f91ccc2688ffa
ProfDema <ilir.dema@mail.utoronto.ca>
2017-05-21 13:16:32 -0400

    Fixed typo

--- E2Starter/src/util/Constants.java
+++ E2Starter/src/util/Constants.java
@@ -1,9 +1,7 @@
 package util;
 
 public class Constants {
-
 	public static final String TORONTO  = "Toronto";
 	public static final String OTTAWA   = "Ottawa";
 	public static final String MONTREAL = "Montreal";
-	
 }
